6649b51a827881968bbeb6d84b25690e8eb09286
dwebbjcg <63323184+dwebbjcg@users.noreply.github.com>
2020-10-07 21:26:52 -0400

    Added test image to page and directory component

--- Rousseau-Webpage/src/app/app.module.ts
+++ Rousseau-Webpage/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DirectoryComponent } from './directory/directory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DirectoryComponent
   ],
   imports: [
     BrowserModule
